84087c62d2829e0b5178a89d0ab07353effbb2f8
Butchy1596 <dterra0726@gmail.com>
2018-03-10 01:57:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,16 +1,18 @@
+//objects defines an object called `playlist`:
 var playlist =
   {
     artistName:"Madonna",
     songTitle:"like a virgin",
   }
 
-
+//objects updatePlaylist(playlist, artistName, songTitle) adds the `artistName: songTitle` key-value pair to `playlist`:
 function updatePlaylist(playlist, artistName, songTitle){
     playlist[artistName] = songTitle;
 
    return playlist
 }
 
+//objects removeFromPlaylist(playlist, artistName) removes `artistName` from `playlist`:
 function removeFromPlaylist(playlist, artistName){
     delete playlist[artistName];
 
