d0d9d3fa23eb0096e71fb248ffe5fa7f8b22b4fa
Butchy1596 <dterra0726@gmail.com>
2018-03-10 01:13:57 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = [
+  {
+    artist:"Madonna",
+    songTitles:"like a virgin",
+  }
+]
+
+function updatePlaylist(playlist, artistName, songTitle){
+   playlist["Rolling Stones"] = "Rock n Roll";
+
+   return playlist
+}
